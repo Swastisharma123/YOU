fdabedf8b739919e02b7ac2a18c1c5419e2427c5
Swastisharma123 <swastisharma@dldavppcbsei.com>
2024-08-20 22:45:31 +0530

    19/08/2024

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:you/screens/homepage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
